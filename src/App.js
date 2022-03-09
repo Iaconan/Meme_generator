01c1f0d0ef162d096896aec5dfed7ed2813ba5a2
Vincenzo Iacona <vincenzoiacona@hotmail.com>
2022-03-09 21:08:08 +0100

    Comments

--- src/App.js
+++ src/App.js
@@ -73,21 +73,190 @@
 
 // USESTATE
 
+// import React from "react"
+
+// export default function App() {
+
+
+//   const [isImportant, func] = React.useState("Yes")
+//   console.log(isImportant)
+//   return (
+//     <div className="state">
+//       <h1 className="state-title">Is state important to know?</h1>
+//       <div className="state-value">
+//         <h1>{isImportant}</h1>
+//       </div>
+//     </div>
+//   )
+// }
+
+
+
+// import React from "react"
+
+// export default function App() {
+
+
+//   const [count, setCount] = React.useState(0)
+//   function countMinus() {
+//     setCount(count - 1)
+//   }
+  
+//   function countPlus() {
+//     setCount(count + 1 )
+//   }
+//   return (
+//     <div>
+//       <button onClick={countMinus} className="counter-minus">-</button>
+//       <div className="counter-count">
+//         <h1>{count}</h1>
+//       </div>
+//       <button onClick={countPlus} className="counter-plus">+</button>
+//     </div>
+//   )
+
+// }
+
+
+// CallBack
+
+
+// import React from "react"
+
+// export default function App() {
+
+
+//   const [count, setCount] = React.useState(0)
+
+  // Note: if you ever need the old value of state
+  // to help you determine the new value of state,
+  // you should pass a callback function to your 
+  // state setter function instead of using 
+  // state directly. This callback function will 
+  // receive the old value of state as its parameter,
+  // which you can then use to determine your new 
+  // value of state.
+
+
+//   function countMinus() {
+//     setCount(prevCount => prevCount - 1)
+//   }
+  
+//   function countPlus() {
+//     setCount(prevCount => prevCount + 1)
+//   }
+//   return (
+//     <div>
+//       <button onClick={countMinus} className="counter-minus">-</button>
+//       <div className="counter-count">
+//         <h1>{count}</h1>
+//       </div>
+//       <button onClick={countPlus} className="counter-plus">+</button>
+//     </div>
+//   )
+
+// }
+
+
+
+// You have 2 options for what you can pass in to a state 
+// setter function (e.g. `setCount`). What are they?
+
+// a. New value of state (setCount(42))
+// b. Callback function - whatever the callback function returns === new value of state
+
+
+// When would you want to pass the first option (from answer above) 
+// to the state setter function?
+
+// Whenever you don't need the previous value of state to determine 
+// what the new value of state should be.
+
+
+
+// When would you want to pass the second option (from answer above)
+// to the state setter function?
+
+// Whenever you DO need the previous value to determine the new value
+
+// import React from "react"
+
+// export default function App() {
+
+
+//   const isGoingOut = true
+
+//   let answer = isGoingOut === true ? "Yes" : "No"
+  
+
+//   return (
+//     <div className="state">
+//       <h1 className="state-title">Do I feel like going out tonight?</h1>
+//       <div className="state-value">
+//         <h1>{answer}</h1>
+//       </div>
+//     </div>
+//   )
+
+// }
+
+
+// import React from "react"
+
+// export default function App() {
+
+
+
+//   const [isGoingOut, setIsGoingOut] = React.useState(true)
+
+// function changeMind() {
+//   setIsGoingOut(prevState => prevState ? false : true)
+// }
+
+//   return (
+//     <div className="state">
+//       <h1 className="state-title">Do I feel like going out tonight?</h1>
+//       <div onClick={changeMind} className="state-value">
+//         <h1>{isGoingOut ? "Yes" : "No"} </h1>
+//       </div>
+//     </div>
+//   )
+
+// }
+
+
+// COMPLEX STATE: object 
+
 import React from "react"
 
 export default function App() {
+  const [contact, setContact] = React.useState({
+    firstName: "John",
+    lastName: "Doe",
+    phone: "+1 (719) 555-1212",
+    email: "itsmyrealname@example.com",
+    isFavorite: false
+  })
 
 
-  const result = React.useState("Yes")
-  console.log(result)
-  return (
-    <div className="state">
-      <h1 className="state-title">Is state important to know?</h1>
-      <div className="state-value">
-        <h1>{result}</h1>
-      </div>
-    </div>
+  let starIcon = contact.isFavorite ? "star-filled.png" : "star-empty.png" 
+
+
+  function toggleFavorite() {
+    setContact(prevContact => ({
+      ...prevContact,
+      isFavorite: !prevContact.isFavorite
+    }))
+  }
+
+  return(
+    <main>
+      <article className="card">
+        <img className="card-image" src="./images/user.png"/>
+        <div className="card-info">
+          <img onClick={toggleFavorite} className="card-favorite" src="../images/star-empty.png"/>
+        </div>
+      </article>
+    </main>
   )
 }
-
-
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,11 +2,14 @@ import React from "react";
 import memesData from "../memesData";
 
 export default function Meme() {
+
+    const [memeImage, setMemeImage] = React.useState("")
+
+
     function getMemeImage() {
         const memesArray = memesData.data.memes
         const randomNumber = Math.round(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
-        console.log(url)
+        setMemeImage(memesArray[randomNumber].url)
     }
     return (
 
@@ -18,6 +21,8 @@ export default function Meme() {
             <input className="form-input" type="text" class="form-control" placeholder="bottom text" aria-label="input" aria-describedby="basic-addon1"/>
 
             <button onClick={getMemeImage} className="form-button">Get a new meme image</button>
+
+            <img className="meme-image" src={memeImage} alt="memeimage"/>
 
             </div>
         </main>
